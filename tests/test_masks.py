4e02ae2806ec4d7b787fc9ce0d29e8192cb1458d
Roman Myachin <roma.mellon@mail.ru>
2025-01-19 10:45:56 +0500

    tests/test_masks.py

---
+++ tests/test_masks.py
@@ -0,0 +1,34 @@
+import os
+import pytest
+from src.masks import get_mask_card_number, get_mask_account
+
+base_path = "C:\\Users\\Acer\\PycharmProjects\\PythonProject"
+
+full_path = os.path.join(base_path, "tests", "test_masks")
+
+with open(full_path, "r") as file:
+    print(file.read())
+
+@pytest.mark.parametrize(
+    ("number", "new_string"),
+    [
+        ("78236498532646378236", "**8236"),
+        ("65184165146725494498", "**4498"),
+        ("65465161116713485164", "**5164"),
+        ("25258458252512468525", "**8525")
+    ]
+)
+def test_get_mask_card_number(number, new_string):
+    assert get_mask_card_number(number) == new_string
+
+@pytest.mark.parametrize(
+    ("numbers", "new_strings"),
+    [
+        ("7823649853264236", "7823 64** **** 4236"),
+        ("6518416514674498", "6518 41** **** 4498"),
+        ("6546516111675164", "6546 51** **** 5164"),
+        ("2525845825258525", "2525 84** **** 8525")
+    ]
+)
+def test_get_mask_account(numbers, new_strings):
+    assert get_mask_account(numbers) == new_strings
