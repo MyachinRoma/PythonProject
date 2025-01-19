b9e5d08b2e173aebc161e0ee2b73b0045edb0100
Roman Myachin <roma.mellon@mail.ru>
2025-01-19 17:16:23 +0500

    tests/test_widget.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,27 +1,19 @@
-import os
 import pytest
-from src.widget import get_mask_account_card, get_data
 
-base_path = "C:\\Users\\Acer\\PycharmProjects\\PythonProject"
+from src.widget import get_data, get_mask_account_card
 
-full_path = os.path.join(base_path, "tests", "test_widget")
-
-with open(full_path, "r") as file:
-    print(file.read())
 
 @pytest.mark.parametrize(
     ("number", "new_string"),
     [
         ("Счет 12345678901234567890", "Счет **7890"),
         ("Visa 1234567890123456", "Visa 1234 56** **** 3456"),
-    ]
+    ],
 )
 def test_get_mask_account_card(number, new_string):
     assert get_mask_account_card(number) == new_string
 
-@pytest.mark.parametrize(
-    ("data", "new_data"),
-    [("2024-03-11T02:26:18.671407", "11.03.2024")]
-)
+
+@pytest.mark.parametrize(("data", "new_data"), [("2024-03-11T02:26:18.671407", "11.03.2024")])
 def test_get_data(data, new_data):
     assert get_data(data) == new_data
