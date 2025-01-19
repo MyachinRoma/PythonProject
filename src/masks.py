4f5a2a6bf382caef1dd48c2a4a1b9c820caa7cfc
Roman Myachin <roma.mellon@mail.ru>
2025-01-19 20:09:18 +0500

    src/masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,5 @@
 import os
+from typing import Union
 
 base_path = "C:\\Users\\Acer\\PycharmProjects\\PythonProject"
 
@@ -7,13 +8,11 @@ full_path = os.path.join(base_path, "src", "masks.py")
 with open(full_path, "r") as file:
     print(file.read())
 
-from typing import Union
-
 
 def get_mask_card_number(card_num: Union[str]) -> Union[str]:
     """Функция возвращает замаскированный номер карты"""
-    if len(card_num) == 20:
-        return f"**{card_num[-4:]}"
+    if len(card_num) == 16:
+        return f"{card_num[:4]}" " " f"{card_num[4:6]}** ****" " " f"{card_num[-4:]}"
     else:
         return "Данные не верны"
 
@@ -21,6 +20,6 @@ def get_mask_card_number(card_num: Union[str]) -> Union[str]:
 def get_mask_account(accounts_num: Union[str]) -> Union[str]:
     """Функуия возвращает замаскированные номера счета"""
     if accounts_num:
-        return f"{accounts_num[:4]}" " " f"{accounts_num[4:6]}** ****" " " f"{accounts_num[-4:]}"
+        return f"**{accounts_num[-4:]}"
     else:
         return "Данные не верны"
--- src/widget.py
+++ src/widget.py
@@ -9,15 +9,15 @@ full_path = os.path.join(base_path, "src", "widget.py")
 with open(full_path, "r") as file:
     print(file.read())
 
-
+#get_mask_account
 def get_mask_account_card(string: str) -> str:
     """Функция возвращает маскирующие счета и карты"""
     if "Счет" in string:
         account = string[-20:]
-        return string[:-20] + get_mask_card_number(account)
+        return string[:-20] + get_mask_account(account)
     else:
         cardnumber = " ".join(string[-16:].split())
-        return string[:-16] + get_mask_account(cardnumber)
+        return string[:-16] + get_mask_card_number(cardnumber)
 
 
 def get_data(user_date: str) -> str:
