7c3a4a203c8bf7994ed61030d319e19c169daaa6
Roman Myachin <roma.mellon@mail.ru>
2025-02-06 20:24:11 +0500

    corrected src/external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -1,21 +1,20 @@
 import os
-from typing import Any
 
 import requests
 from dotenv import load_dotenv
 
-load_dotenv('.env')
+load_dotenv(".env")
 values = os.getenv("PASSWORD")
 API_KEY = os.getenv("API_KEY")
 headers = {API_KEY: values}
 
 
-def currency_conversion(transaction: Any) -> Any:
+def currency_conversion(transaction: dict) -> float:
     """Функция конвертации"""
-    amout = transaction["operationAmount"]["amount"]
+    amount = transaction["operationAmount"]["amount"]
     code = transaction["operationAmount"]["currency"]["code"]
     to = "RUB"
-    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={code}&amount={amout}"
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={code}&amount={amount}"
     payload = {}
     response = requests.get(url, headers={"apikey": values}, data=payload)
     result = response.json()
