fb1e499bc64ca963c87ff134cca9b4717b2d0c50
Roman Myachin <roma.mellon@mail.ru>
2025-02-11 22:35:59 +0500

    corrected the logs

--- src/masks.py
+++ src/masks.py
@@ -1,51 +1,43 @@
-import os
 import logging
+import os
 from typing import Union
 
 base_path = os.path.abspath(os.path.dirname(__file__))
 full_path = os.path.join(base_path, "masks.py")
-logs_path = os.path.join(base_path, "logs/masks.log")
+logs_path = os.path.join(base_path, "..", "logs", "masks.log")
+if not os.path.exists(os.path.dirname(logs_path)):
+    os.makedirs(os.path.dirname(logs_path))
 
-with open(full_path, "r") as file:
-    print(file.read())
+# with open(full_path, "r") as file:
+# print(file.read())
 
 
-app_logger = logging.getLogger("masks")
-file_handler = logging.FileHandler("logs/masks.log", encoding="utf-8", mode='w')
-file_formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+app_logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler(logs_path, encoding="utf-8", mode="w")
+file_formatter = logging.Formatter("%(levelname)s: %(name)s:Request time: %(asctime)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 app_logger.addHandler(file_handler)
 app_logger.setLevel(logging.DEBUG)
-
-
-app_logger.debug('Debug message')
-app_logger.info('Info message')
-app_logger.warning('Warning message')
-app_logger.error('Error message')
-app_logger.critical('Critical message')
+app_logger.propagate = False
 
 
 def get_mask_card_number(card_num: Union[str]) -> Union[str]:
     """Функция возвращает замаскированный номер карты"""
-    app_logger.info("Initialization")
+    app_logger.info("Идет проверка количества заданных символов")
     if len(card_num) == 16:
-        app_logger.info("Finish")
+        app_logger.info("Выполняется маскировка номера карты")
         return f"{card_num[:4]}" " " f"{card_num[4:6]}** ****" " " f"{card_num[-4:]}"
     else:
-        app_logger.critical("Данные не верны")
+        app_logger.error("Данные не верны")
         return "Данные не верны"
 
 
 def get_mask_account(accounts_num: Union[str]) -> Union[str]:
     """Функуия возвращает замаскированные номера счета"""
-    app_logger.info("Initialization")
+    app_logger.info("Идет проверка количества заданных символов")
     if len(accounts_num) == 20:
-        app_logger.info("Finish")
+        app_logger.info("Выполняется маскировка номера счета")
         return f"**{accounts_num[-4:]}"
     else:
-        app_logger.critical("Данные не верны")
+        app_logger.error("Данные не верны")
         return "Данные не верны"
-
-if __name__ == "__main__":
-    print(get_mask_card_number("7000561278963206"))
-    print(get_mask_account("15369785463259863240"))
--- src/utils.py
+++ src/utils.py
@@ -1,63 +1,53 @@
-import os
 import json
 import logging
+import os
 from json import JSONDecodeError
 from typing import Any
+
 from src.external_api import currency_conversion
 
 base_path = os.path.abspath(os.path.dirname(__file__))
 full_path = os.path.join(base_path, "utils.py")
-logs_path = os.path.join(base_path, "logs/utils.log")
+logs_path = os.path.join(base_path, "..", "logs", "utils.log")
+if not os.path.exists(os.path.dirname(logs_path)):
+    os.makedirs(os.path.dirname(logs_path))
 
 with open(full_path, "r") as file:
     print(file.read())
 
-app_logger = logging.getLogger("utils")
-file_handler = logging.FileHandler("logs/utils.log", encoding="utf-8", mode='w')
-file_formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+
+app_logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler(logs_path, encoding="utf-8", mode="w")
+file_formatter = logging.Formatter("%(levelname)s: %(name)s:Request time: %(asctime)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 app_logger.addHandler(file_handler)
 app_logger.setLevel(logging.DEBUG)
-
-
-app_logger.debug('Debug message')
-app_logger.info('Info message')
-app_logger.warning('Warning message')
-app_logger.error('Error message')
-app_logger.critical('Critical message')
+app_logger.propagate = False
 
 
 def financial_transactions(path: str) -> list:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
-    app_logger.info("Initialization")
     try:
-        app_logger.info("Initialization")
         with open(path, encoding="utf-8") as financial_file:
-            app_logger.info("Initialization")
+            app_logger.info("Файл успешно открыт")
             try:
                 transactions = json.load(financial_file)
-                app_logger.info("Initialization")
+                app_logger.info("Содержимое файла преобразовано в объект")
             except JSONDecodeError:
-                app_logger.info("Finish")
+                app_logger.error("Ошибка декодирования файла")
                 return []
-        app_logger.info("Initialization")
         if not isinstance(transactions, list):
-            app_logger.info("Finish")
             return []
-        app_logger.info("Finish")
         return transactions
-        app_logger.critical("FileNotFoundError")
     except FileNotFoundError:
+        app_logger.error("Ошибка декодирования файла")
         return []
 
 
 def transaction_amount(trans: dict, currency: str = "RUB") -> Any:
     """Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
-    app_logger.info("Initialization")
     if trans["operationAmount"]["currency"]["code"] == currency:
         amount = trans["operationAmount"]["amount"]
-        app_logger.info("Initialization")
     else:
         amount = currency_conversion(trans)
-        app_logger.info("Finish")
     return amount
